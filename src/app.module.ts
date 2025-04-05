9834cb80fe8e57f509e417fec016c09a6b825a68
hus0316 <hus0316@ariatech.kr>
2025-04-05 16:10:58 +0900

    typeOrm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot({
+    type : 'sqlite',
+    database : 'db.sqlite',
+    entities : [User],
+    synchronize : true,
+  }), 
+  UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
