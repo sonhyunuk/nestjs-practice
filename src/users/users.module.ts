7ef20cee9a2be19bc28c04ec97dc4bd6c2be5ad6
손현욱 <hus0316@ariatech.kr>
2025-04-08 14:22:11 +0900

    user create service 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { User } from './user.entity';
+import { User } from './users.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
