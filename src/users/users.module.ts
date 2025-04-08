293e8bfe3254c2066fba7c354bf877594ba49252
손현욱 <hus0316@ariatech.kr>
2025-04-08 11:28:41 +0900

    TypeOrmModule 적용 및 entity 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users.entity';
+import { User } from './user.entity';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
