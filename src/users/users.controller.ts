f7cfc100cd44b43b19d02eaf5ea6f7fab147c11b
손현욱 <hus0316@ariatech.kr>
2025-04-16 11:58:56 +0900

    로그인 안될때 처리를 위한 guard 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
     Post, 
     Query,
     Session,
+    UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UpdateUserDto } from './dtos/update-user-dto';
@@ -18,7 +19,7 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
-
+import { AuthGuard } from 'src/guards/auth.guard';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
@@ -28,6 +29,7 @@ export class UsersController {
         private AuthService: AuthService
     ) {}
 
+    @UseGuards(AuthGuard)
     @Get('/whoami')
     whoAmI(@CurrentUser() user : User) {
 
